dca91b5518a9caf718a05468d0ca40dca76998ce
Vincenzo <vincedinardo21@gmail.com>
2022-05-04 16:01:54 +0200

    Correzioni

--- js/myscript.js
+++ js/myscript.js
@@ -13,12 +13,12 @@ let sconto;
 let prezzoAttuale;
 
 // Controllo l'età del passeggero
-if(eta < 18){
+if (eta < 18) {
     console.log("Il passeggero è minorenne, sconto 20%");
     sconto = (prezzo * 20) / 100;
 }
 
-else if(eta >= 65){
+else if (eta >= 65) {
     console.log("Il passeggero ha più di 65 anni, sconto 40%");
     sconto = (prezzo * 40) / 100;
 }
@@ -32,6 +32,7 @@ else {
 prezzoAttuale = prezzo - sconto;
 console.log(prezzoAttuale);
 
+// Arrotondo il prezzo a due cifre decimali
 let rounded = Math.round((prezzoAttuale + Number.EPSILON) * 100) / 100;
 
 // Stampo il pezzo del biglietto
