d95175640a1953de3d7d7fc7fb6f8dbf2fc359c3
Vincenzo <vincedinardo21@gmail.com>
2022-05-04 15:29:12 +0200

    Stampo prezzo da sistemare

--- js/myscript.js
+++ js/myscript.js
@@ -6,20 +6,29 @@ console.log(numeroKm);
 const eta = parseInt(prompt("Inserire l'età"));
 console.log(eta);
 
+let prezzo = (numeroKm * 0.21);
+let sconto;
+let prezzoAttuale;
+
 // Controllo l'età del passeggero
 if(eta < 18){
     console.log("Il passeggero è minorenne, sconto 20%");
+    sconto = (prezzo * 20) / 100;
 }
 
 else if(eta >= 65){
     console.log("Il passeggero ha più di 65 anni, sconto 40%");
+    sconto = (prezzo * 40) / 100;
 }
 
 else {
     console.log("Il passeggero non ha nessuno sconto");
+    sconto = 0;
 }
 
 // Calcolo il prezzo totale del viaggio
-
+prezzoAttuale = prezzo - sconto;
+console.log(prezzoAttuale);
 
 // Stampo il pezzo del biglietto
+document.getElementById("prezzo-id").innerHTML = "Il prezzo totale del biglietto è: " + prezzoAttuale + " euro";
